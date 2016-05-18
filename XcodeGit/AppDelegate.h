9e6d2018c1645efcba912b8941c29e9ebc313014
swy <swyhei8080@qq.com>
2016-05-18 09:54:20 +0800

    init commit

---
+++ XcodeGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XcodeGit
+//
+//  Created by shi on 16/5/18.
+//  Copyright © 2016年 finefor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XcodeGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XcodeGit
+//
+//  Created by shi on 16/5/18.
+//  Copyright © 2016年 finefor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
